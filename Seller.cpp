788e3e5889a86620ce44d84e89bc383ed1fbfbaf
PatrickBarber <patrickbarber61@yahoo.com>
2016-12-01 19:03:45 -0500

    Update Seller.cpp

--- Seller.cpp
+++ Seller.cpp
@@ -1,10 +1,6 @@
 #include<iostream>
 #include<vector>
-#include<queue>
-#include<cmath>
 #include"Seller.hpp"
-#include"Buyer.hpp"
-#include"BuyerSellerTree.hpp"
 
 Seller::Seller(int l[], double min, double max, double ex)
 {
@@ -14,38 +10,6 @@ Seller::Seller(int l[], double min, double max, double ex)
 	maxDist = max;
 	exRate = ex;
 }
-//Don't know the order in the main for when to implement this
-double Seller::calculateDistanceToBuyer(int buyerLocation[])
-{
-	int temp = 0;
-	for (int i = 0; i < locationCoordinateSize; i++)
-		temp += pow(std::abs(location[i] - buyerLocation[i]), 2);
-	distanceToBuyer= sqrt(temp);
-}
-
-double Seller::getDistanceToBuyer()
-{
-	return distanceToBuyer;
-}
-
-
-void Seller::setBuyerList(std::vector<Buyer*> buyList)
-{
-	for (int i = 0; i < buyList.size; i++)
-		buyerList[i] = buyList[i];
-}
-
-/*
-not implemented yet
-void Seller::removeOutOfRangeValues()
-{
-}
-*/
-
-/*
-Buyer:: selectMatch()
-*/
-
 
 double Seller::getMaxDist()
 {
