e1aed5a7078138e029ab4c7134a228d3a40bcb3a
PatrickBarber <patrickbarber61@yahoo.com>
2016-12-02 18:16:27 -0500

    Update SortAndCalculate.cpp

--- SortAndCalculate.cpp
+++ SortAndCalculate.cpp
@@ -10,6 +10,61 @@ SortAndCalculate::SortAndCalculate(Seller *s, vector<Buyer*> b)
 	seller = s;
 	buyerList = b;
 }
+
+void SortAndCalculate::buyerFlow()
+{	
+	//calculates the distance each seller must go to reach the buyer
+	calculateDistanceToBuyer();
+	//removes all the sellers who do not match the buyer
+	removeOutOfRangeValues(buyer, sellerList);
+	//Sorts the seller list and stores the sorted sellerlist into sorted sellerlist
+	//I will need a parameter to know if I should sort with respect to distance or price
+	sortSellerList();
+	//returns the lowest price or distance
+	getSelectedSeller();
+
+}
+
+void SortAndCalculate::sellerFlow()
+{
+	calculateDistanceToSeller();
+	removeOutOfRangeValues(seller, buyerList);
+	sortBuyerList();
+	getSelectedBuyer();
+}
+
+//Not tested at all
+void SortAndCalculate::calculateDistanceToBuyer()
+{
+	double temp = 0;
+
+	for (unsigned int j = 0; j < sellerList.size(); j++)
+	{
+		for (unsigned int i = 0; i < buyer->getLocationCoordinateSize(); i++)
+		{
+			temp += pow(std::abs(sellerList[j]->getLocation()[i] - buyer->getLocation()[i]), 2);
+		}
+		sellerList[j]->setDistanceToBuyer(temp);
+		temp = 0;
+	}
+}
+//not tested
+void SortAndCalculate::calculateDistanceToSeller()
+{
+	double temp = 0;
+
+	for (unsigned int j = 0; j < buyerList.size(); j++)
+	{
+		for (unsigned int i = 0; i < seller->getLocationCoordinateSize(); i++)
+		{
+			temp += pow(std::abs(buyerList[j]->getLocation()[i] - seller->getLocation()[i]), 2);
+		}
+		buyerList[j]->setDistanceToSeller(temp);
+		temp = 0;
+	}
+}
+
+
 //Removes the sellers whose requirements do not match the buyers
 void SortAndCalculate::removeOutOfRangeValues(Buyer *b, vector<Seller*> s)
 {
@@ -28,14 +83,18 @@ void SortAndCalculate::removeOutOfRangeValues(Seller *s, vector<Buyer*> b)
 			b.erase(b.begin() + i);
 	}
 }
-
+//Might pass a parameter to indicate if it is to be sorted by exrate or distance
 void SortAndCalculate::sortSellerList()
 {
 	tree *elm = new tree();
 
 	for (unsigned int i = 0; i < sellerList.size(); i++)
 		elm->addLeaf(sellerList[i]->getExcangeRate(), sellerList[i]);
-
+	/*
+	if(distance )
+	for (unsigned int i = 0; i < sellerList.size(); i++)
+		elm->addLeaf(sellerList[i]->getDistanceToBuyer(), sellerList[i]);
+	*/
 	elm->inOrderSellerList(elm->root);
 	for (unsigned int i = 0; i < sellerList.size(); i++)
 		sortedSellerList.push_back(elm->getSortedSellerList()[i]);
@@ -47,7 +106,11 @@ void SortAndCalculate::sortBuyerList()
 
 	for (unsigned int i = 0; i < buyerList.size(); i++)
 		oak->addLeaf(buyerList[i]->getExcangeRate(), buyerList[i]);
-
+	/*
+	if(distance or whatever)
+	for (unsigned int i = 0; i < buyerList.size(); i++)
+		oak->addLeaf(buyerList[i]->getDistanceToSeller(), buyerList[i]);
+	*/
 	oak->inOrderSellerList(oak->root);
 
 	for (unsigned int i = 0; i < buyerList.size(); i++)
