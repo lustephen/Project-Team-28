ff729351f3b79bcb3dd7f7085ab9033d3e096a1b
PatrickBarber <patrickbarber61@yahoo.com>
2016-12-01 19:04:35 -0500

    Update BuyerSellerTree.hpp

--- BuyerSellerTree.hpp
+++ BuyerSellerTree.hpp
@@ -1,31 +1,33 @@
-class tree 
+class tree
 {
 private:
 
 	struct node
 	{
-		int data;
+		double data;
 		Buyer* whichBuyer;
 		Seller* whichSeller;
 		node* left;
 		node* right;
 	};
 
-	node* root;
-	node* makeLeaf(int data,Buyer *b);
-	void addLeaf(int data, node* p,Buyer *b);
+	
+	node* makeLeaf(double data, Buyer *b);
+	void addLeaf(double data, node* p, Buyer *b);
 
-	node* makeLeaf(int data, Seller* s);
-	void addLeaf(int data, node* p, Seller* s);
-	std::vector<Seller*> sortedSellerList;
-	std::vector<Buyer*> sortedBuyerList;
+	node* makeLeaf(double data, Seller* s);
+	void addLeaf(double data, node* p, Seller* s);
+	vector <Seller*> sortedSellerList;
+	vector<Buyer*> sortedBuyerList;
 public:
+	node* root;
 	tree();
-	void addLeaf(int data, Buyer* b);
-	void addLeaf(int data, Seller* s);
+	//node* getRoot();
+	void addLeaf(double data, Buyer* b);
+	void addLeaf(double data, Seller* s);
 	void inOrderBuyerList(node* n);
 	void inOrderSellerList(node* n);
-	std::vector<Seller*> getSortedSellerList();
-	std::vector<Buyer*> getSortedBuyerList();
+	vector<Seller*> getSortedSellerList();
+	vector<Buyer*> getSortedBuyerList();
 
 };
