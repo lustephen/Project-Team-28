d530a7260a6047b3d93c48b76af8756b67c01c0b
PatrickBarber <patrickbarber61@yahoo.com>
2016-12-01 19:06:43 -0500

    Create SortAndCalculate.hpp

---
+++ SortAndCalculate.hpp
@@ -0,0 +1,23 @@
+#include<vector>
+#include"Seller.hpp"
+#include"Buyer.hpp"
+class SortAndCalculate
+{
+	Buyer *buyer;
+	Seller *seller;
+	vector<Seller*> sellerList;
+	vector<Buyer*> buyerList;
+	vector<Seller*> sortedSellerList;
+	vector<Buyer*> sortedBuyerList;
+	void sortSellerList();
+	void sortBuyerList();
+	void removeOutOfRangeValues(Buyer *b, vector<Seller*> s);
+	void removeOutOfRangeValues(Seller *s, vector<Buyer*> b);
+
+public:
+	SortAndCalculate(Buyer *b, vector<Seller*> s);
+	SortAndCalculate(Seller *s, vector<Buyer*> b);
+	Buyer* getSelectedBuyer();
+	Seller* getSelectedSeller();
+	
+};
