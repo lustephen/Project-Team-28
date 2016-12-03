fda81b03da965fd2bdf97ba41c259a8c3e150d73
Danilo Souza <danilos063@gmail.com>
2016-12-03 02:36:32 -0500

    Create Settings.cpp

---
+++ Settings.cpp
@@ -0,0 +1,56 @@
+#include "Settings.h"
+
+void Settings::setMaxDistance(double max)
+{
+	maxdistance = max;
+}
+
+void Settings::setExchangeRate(double exchange)
+{
+	exchangerate = exchange;
+}
+
+void Settings::setMinDistance(double min)
+{
+	mindistance = min;
+}
+
+void Settings::setMinExchangeRate(double minexchange)
+{
+	minexchangerate = minexchange;
+}
+
+void Settings::setMinProfit(double minprof)
+{
+	minprofit = minprof;
+}
+
+void Settings::setMaxDistance(double max)
+{
+	maxdistance = max;
+}
+
+double Settings::getMaxDistance()
+{
+	return maxdistance;
+}
+
+double Settings::getExchangeRate()
+{
+	return exchangerate;
+}
+
+double Settings::getMinDistance()
+{
+	return mindistance;
+}
+
+double Settings::getMinExchangeRate()
+{
+	return minexchangerate;
+}
+
+double Settings::getMinProfit()
+{
+	return minprofit;
+}
