9ea81b946b69472bde6e6a2e0ec774db0b5fec0d
Danilo Souza <danilos063@gmail.com>
2016-12-03 12:34:05 -0500

    Update Settings.hpp

--- Settings.hpp
+++ Settings.hpp
@@ -5,15 +5,12 @@ class Settings
 {
 	private:
 		double exchangerate;
-		double minexchangerate;
 		double minprofit;
 		
 	public:
 		void setExchangeRate(double exchange);
-		void setMinExchangeRate(double minexchange);
 		void setMinProfit(double minprof);
 		
 		double getExchangeRate();
-		double getMinExchangeRate();
 		double getMinProfit();
 };
