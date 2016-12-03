c54b9fb87674b33a351c05826e10d441a52d0d60
PatrickBarber <patrickbarber61@yahoo.com>
2016-12-03 10:28:58 -0500

    Removed distance and added a constructor

--- Buyer.hpp
+++ Buyer.hpp
@@ -1,21 +1,22 @@
-class Buyer//: public Person
+class Buyer: public Person
 {
 	const int locationCoordinateSize = 2;
-	int location[2];
+	//int location[2];
 	double estPurchase;
-	double maxDist;
+	//double maxDist;
 	double exRate;
-	double distanceToSeller;
+	//double distanceToSeller;
 	
 public:
-	Buyer(int l[], double est, double max, double ex);
+	Buyer(double est, double ex);
+	Buyer(double est, double ex, int mm, int dd, int yr, std::string n, char gender, int ufid);
 	
 
-	double getMaxDist();
+	//double getMaxDist();
 	double getEstPurchase();
 	double getExcangeRate();
-	int* getLocation();
-	int getLocationCoordinateSize();
-	void setDistanceToSeller(double dist);
-	double getDistanceToSeller();
+	//int* getLocation();
+	//int getLocationCoordinateSize();
+	//void setDistanceToSeller(double dist);
+	//double getDistanceToSeller();
 };
