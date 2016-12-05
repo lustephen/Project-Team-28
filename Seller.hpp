ae3c81c93160a92811817b1acf2f938136b5d4b3
PatrickBarber <patrickbarber61@yahoo.com>
2016-12-05 00:20:50 -0500

    removed commented out code

--- Seller.hpp
+++ Seller.hpp
@@ -2,37 +2,23 @@
 #define Seller_hpp
  
 #include "Person.hpp"
-
+/*
+Creates a seller with the fields minimum purchase and exchange rate
+*/
 class Seller: public Person
 {
-	//const int locationCoordinateSize = 2;
-	//int location[2];
+	
 	double minPurchase;
-	//double maxDist;
 	double exRate;
-	//double distanceToBuyer;
-
 
 public:
 	Seller(double minPurchase, double exRate);
 	Seller( double minPurchase, double exRate, int mm, int dd, int yr, std::string n, char gender, std::string ufid);
 	double getMinPurchase();
-	//double getMaxDist();
 	double getExchangeRate();
-	//int getLocationCoordinateSize();
-	//int* getLocation();
-	//void setDistanceToBuyer(double dist);
-	//double getDistanceToBuyer();
 
 };
 
-// void createSellerPop(std::vector<Seller*> vec) {
-// 	for (int h = 0; h < 20; h++) {
-// 		int j = randMonth();
-// 		char z = randGender();
-// 		Seller *sell = new Seller(randMinPurchase(), randExRate(), j, randDay(j), randYear(), randName(z), z, randUfid());
-// 		vec.push_back(sell);
-// 	}
-// }
+/
 
 #endif
