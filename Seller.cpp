6fa7bb022cd58c67e001c96763936b8e2fe65722
PatrickBarber <patrickbarber61@yahoo.com>
2016-12-05 00:18:35 -0500

    removed commented out code

--- Seller.cpp
+++ Seller.cpp
@@ -7,30 +7,18 @@ using namespace std;
 
 Seller::Seller(double min, double ex)
 {
-	//for (int i = 0; i < locationCoordinateSize; i++)
-		//location[i] = l[i];
 	minPurchase = min;
-	//maxDist = max;
 	exRate = ex;
 	Person();
 }
+
 Seller::Seller(double min, double ex, int mm, int dd, int yr, string n, char u, string ufid) : Person(n, mm, dd, yr, u, ufid)
 {
 	minPurchase = min;
 	exRate = ex;
 	Person(n,mm,dd,yr,u,ufid);
 }
-/*
-void Seller::setDistanceToBuyer(double dist)
-{
-	distanceToBuyer = dist;
-}
 
-double Seller::getMaxDist()
-{
-	return maxDist;
-}
-*/
 double Seller::getMinPurchase()
 {
 	return minPurchase;
@@ -40,20 +28,5 @@ double Seller::getExchangeRate()
 {
 	return exRate;
 }
-/*
-int Seller::getLocationCoordinateSize()
-{
-	return locationCoordinateSize;
-}
 
 
-int* Seller::getLocation()
-{
-	return location;
-}
-
-double Seller::getDistanceToBuyer()
-{
-	return distanceToBuyer;
-}
-*/
