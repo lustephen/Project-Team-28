cb40279cedfe76f2559fbcc76acb9b674f1d7cdd
PatrickBarber <patrickbarber61@yahoo.com>
2016-12-05 00:16:51 -0500

    removed commented out code and const int coordinate size

--- Buyer.hpp
+++ Buyer.hpp
@@ -2,38 +2,25 @@
 #define Buyer_hpp
 
 #include "Person.hpp"
-
+/*
+Creates a buyer who has the fields of estimated purchase and exchange rate
+*/
 class Buyer: public Person
 {
-	const int locationCoordinateSize = 2;
-	//int location[2];
 	double estPurchase;
-	//double maxDist;
 	double exRate;
-	//double distanceToSeller;
-
+	
 public:
 	Buyer(double est, double ex);
 	Buyer(double est, double ex, int mm, int dd, int yr, std::string n, char gender, std::string ufid);
 
 
-	//double getMaxDist();
+	
 	double getEstPurchase();
 	double getExchangeRate();
-	//int* getLocation();
-	//int getLocationCoordinateSize();
-	//void setDistanceToSeller(double dist);
-	//double getDistanceToSeller();
+	
 };
 
-// void createBuyerPop(std::vector<Buyer*> vec) {
-// 	for (int g = 0; g < 20; g++) {
-// 		int a = randMonth();
-// 		char b = randGender();
-// 		Buyer *buy = new Buyer(randEstPurchase(), randExRate(), a, randDay(a), randYear(), randName(b), b, randUfid());
-// 		vec.push_back(buy);
-//
-// 	}
-// }
+
 
 #endif
